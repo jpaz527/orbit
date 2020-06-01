9c2444f62ad7364a9560627e5fb71c160f064d95
jpaz <jpaz_ar@hotmail.com>
2020-05-31 20:10:26 -0600

    Added logout functionality with redirect to login.

--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -1,9 +1,11 @@
 import React, { useState, createContext } from "react";
+import { useHistory } from "react-router-dom";
 
 const AuthContext = createContext();
 const { Provider } = AuthContext;
 
 const AuthProvider = ({ children }) => {
+  const history = useHistory();
   const token = localStorage.getItem("token");
   const userInfo = localStorage.getItem("userInfo");
   const expiresAt = localStorage.getItem("expiresAt");
@@ -20,13 +22,25 @@ const AuthProvider = ({ children }) => {
     setAuthState({ token, userInfo, expiresAt });
   };
 
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userInfo");
+    localStorage.removeItem("expiresAt");
+    setAuthState({
+      token: null,
+      expiresAt: null,
+      userInfo: {},
+    });
+    history.push("/login");
+  };
+
   const isAuthenticated = () => {
     const { token, expiresAt } = authState;
     if (!token || !expiresAt) return false;
 
     const isAuthValid = new Date().getTime() / 1000 < expiresAt;
     console.log(`Authentication valid? ${isAuthValid}`);
-    
+
     return isAuthValid;
   };
 
@@ -36,6 +50,7 @@ const AuthProvider = ({ children }) => {
         authState,
         setAuthState: (authInfo) => setAuthInfo(authInfo),
         isAuthenticated,
+        logout,
       }}
     >
       {children}
