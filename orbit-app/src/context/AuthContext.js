6d1ab5733eba1bd65018fe276da3c3516311a274
jpaz <jpaz_ar@hotmail.com>
2020-05-31 19:43:23 -0600

    Updated redirection based on authentication state.

--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -12,17 +12,30 @@ const AuthProvider = ({ children }) => {
     expiresAt,
     userInfo: userInfo ? JSON.parse(userInfo) : {},
   });
+
   const setAuthInfo = ({ token, userInfo, expiresAt }) => {
     localStorage.setItem("token", token);
     localStorage.setItem("userInfo", JSON.stringify(userInfo));
     localStorage.setItem("expiresAt", expiresAt);
     setAuthState({ token, userInfo, expiresAt });
   };
+
+  const isAuthenticated = () => {
+    const { token, expiresAt } = authState;
+    if (!token || !expiresAt) return false;
+
+    const isAuthValid = new Date().getTime() / 1000 < expiresAt;
+    console.log(`Authentication valid? ${isAuthValid}`);
+    
+    return isAuthValid;
+  };
+
   return (
     <Provider
       value={{
         authState,
         setAuthState: (authInfo) => setAuthInfo(authInfo),
+        isAuthenticated,
       }}
     >
       {children}
